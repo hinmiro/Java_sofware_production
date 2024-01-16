71a2efa1cd0fbfd4f767e98abcc6594dbd96d458
mirohi <m_hintikka@hotmail.com>
2024-01-16 11:45:11 +0200

    Task 2

---
+++ src/module01/Task02.java
@@ -0,0 +1,10 @@
+package module01;
+
+public class Task02 {
+    public static void main(String[] args) {
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+    }
+}
