20d0e99802d84f170596294ad2de20220912c446
mirohi <m_hintikka@hotmail.com>
2024-01-20 14:12:26 +0200

    tehtavasarja1_tehtava4

--- src/ProgrammingProject/Asiakas.java
+++ src/ProgrammingProject/Asiakas.java
@@ -1,5 +1,9 @@
 package ProgrammingProject;
 
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Asiakas {
 
     private static int latestId = 1;
@@ -9,6 +13,7 @@ public class Asiakas {
     public Asiakas() {
         this.id = latestId++;
         this.start = setter();
+
     }
 
     long getter() {
@@ -19,16 +24,39 @@ public class Asiakas {
         return System.currentTimeMillis();
     }
 
-    void kulutettuAika() {
+    public long kulutettuAika() {
         this.end = getter();
-        System.out.printf("Client %s used time %s\n", this.id, (this.end - this.start));
+        return this.end - this.start;
     }
 
     public static void main(String[] args) {
-        Asiakas asiakas = new Asiakas();
-        Asiakas asiakas1 = new Asiakas();
-        asiakas.kulutettuAika();
-        asiakas1.kulutettuAika();
+        int userInput = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        LinkedList<Object> jono = new LinkedList<Object>();
+        while (true) {
+            System.out.println("Lisää jonoon (1)\nPoista jonosta(2)\nLopeta(3)");
+            userInput = Integer.parseInt(scanner.nextLine());
+
+            if (userInput == 1) {
+                Asiakas asiakas = new Asiakas();
+                jono.add(asiakas);
+            }
+
+            else if (userInput == 2) {
+                Object obj = jono.poll();
+
+                Asiakas asiakas = (Asiakas) obj;
+                long time = asiakas.kulutettuAika();
+                System.out.printf("\nKulutettu aika %s\n", time);
+
+            }
+
+            else if (userInput == 3) {
+
+                break;
+            }
+        }
     }
 
 }
