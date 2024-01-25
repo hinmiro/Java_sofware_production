568e473594ac763e42c132febfe3e31f607016fa
mirohi <m_hintikka@hotmail.com>
2024-01-25 20:21:08 +0200

    module02 task02

--- src/module02/Library.java
+++ src/module02/Library.java
@@ -3,13 +3,14 @@ package module02;
 import java.util.ArrayList;
 
 public class Library {
-    private static final ArrayList<Book> books = new ArrayList<>();
+    private final ArrayList<Book> books = new ArrayList<>();
+    private final ArrayList<Book> borrowedBooks = new ArrayList<>();
 
-    public static void addBook(Book book) {
+    public void addBook(Book book) {
         books.add(book);
     }
 
-    public static void displayBooks() {
+    public void displayBooks() {
         int increment = 1;
         System.out.println("Library Catalog:");
         for (Book book : books) {
@@ -17,7 +18,7 @@ public class Library {
         }
     }
 
-    public static void findBooksByAuthor(String name) {
+    public void findBooksByAuthor(String name) {
         System.out.printf("\nBooks by Author \"%s\":\n", name);
         for (Book book : books) {
             if (book.getAuthor().equals(name)) {
@@ -26,15 +27,36 @@ public class Library {
         }
     }
 
+    public void borrowBook(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equals(title)) {
+                books.remove(book);
+                borrowedBooks.add(book);
+            }
+        }
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+        books.add(book);
+    }
+
     public static void main(String[] args) {
+        Library oodi = new Library();
         Book book1 = new Book("The fellowship of the ring", "J.R.R. Tolkien", 1954);
         Book book2 = new Book("Two towers", "J.R.R. Tolkien", 1954);
         Book book3 = new Book("The return of the king", "J.R.R. Tolkien", 1955);
-        addBook(book1);
-        addBook(book2);
-        addBook(book3);
-        displayBooks();
-        findBooksByAuthor("J.R.R. Tolkien");
+        oodi.addBook(book1);
+        oodi.addBook(book2);
+        oodi.addBook(book3);
+        oodi.displayBooks();
+        oodi.findBooksByAuthor("J.R.R. Tolkien");
+        oodi.borrowBook("Two towers");
+        System.out.println("\n\n");
+        oodi.displayBooks();
+        oodi.returnBook(oodi.borrowedBooks.get(0));
+        System.out.println("\n\n");
+        oodi.displayBooks();
 
     }
 }
