6292ba66cb00060c277d3085208730b351d7deb6
mirohi <m_hintikka@hotmail.com>
2024-02-03 11:45:31 +0200

    module3 fibonacci sequence

---
+++ src/module03/datastreams/FobonacciCsv.java
@@ -0,0 +1,33 @@
+package module03.datastreams;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class FobonacciCsv {
+    static ArrayList<BigInteger> sequence = new ArrayList<>();
+
+    public static void main(String[] args) {
+        sequence.add(BigInteger.ZERO);
+        sequence.add(BigInteger.ONE);
+        for (int i = 2; i < 100; i++) {
+            BigInteger newFibonacci = sequence.get(i - 1).add(sequence.get(i - 2));
+            sequence.add(newFibonacci);
+        }
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("fiboNacci.csv"));
+            bufferedWriter.write("Fibonacci Sequence");
+            bufferedWriter.newLine();
+
+            for (BigInteger bigInt : sequence) {
+                bufferedWriter.write(String.valueOf(bigInt));
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.close();
+        } catch (IOException e) {
+            System.err.print(e);
+        }
+    }
+}
