021e428df635444400b102c41753ea2e8370f4ba
mirohi <m_hintikka@hotmail.com>
2024-02-14 11:36:45 +0200

    module5 task TicketReservationSystem

---
+++ src/module05/TicketReservationSystem.java
@@ -0,0 +1,57 @@
+package module05;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TicketReservationSystem {
+    private static int seats = 10;
+
+    public synchronized void reserve(int tickets, long id) {
+        if (seats - tickets > 0) {
+            seats -= tickets;
+            System.out.printf("\nCustomer %s reserved %s tickets", id, tickets);
+        } else {
+            System.out.printf("\nCustomer %s couldn't reserve %s tickets.", id, tickets);
+        }
+    }
+
+    public static void main(String[] args) {
+        TicketReservationSystem trs = new TicketReservationSystem();
+        ArrayList<Thread> customers = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            Thread t = new Thread(new Customer(trs));
+            customers.add(t);
+        }
+        for (Thread t : customers) {
+            t.start();
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+}
+
+
+class Customer implements Runnable {
+    public static Random rand = new Random();
+
+    private static int increment = 1;
+    private int id;
+    private TicketReservationSystem ticketReservationSystem;
+
+    public Customer(TicketReservationSystem t) {
+        this.ticketReservationSystem = t;
+        this.id = increment++;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public void run() {
+        ticketReservationSystem.reserve(rand.nextInt(3) + 1, this.id);
+    }
+}
+
